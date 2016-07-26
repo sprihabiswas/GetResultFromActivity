2ecd0340cc62676c2622a78d9834bc8b5b36ba24
Spriha Biswas <sprihabiswas@gmail.com>
2016-07-26 14:29:44 +0530

    Initial commit

---
+++ app/src/main/java/com/support/android/getresultfromactivity/MainActivity.java
@@ -0,0 +1,70 @@
+package com.support.android.getresultfromactivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    static final int PICK_CONTACT_REQUEST = 1;  // The request code
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void getContact(View v)
+    {
+        pickContact();
+    }
+
+    private void pickContact() {
+        Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+        startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request it is that we're responding to
+        if (requestCode == PICK_CONTACT_REQUEST) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // Get the URI that points to the selected contact
+                Uri contactUri = data.getData();
+                // We only need the NUMBER column, because there will be only one row in the result
+                String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+                // Perform the query on the contact to get the NUMBER column
+                // We don't need a selection or sort order (there's only one result for the given URI)
+                // CAUTION: The query() method should be called from a separate thread to avoid blocking
+                // your app's UI thread. (For simplicity of the sample, this code doesn't do that.)
+                // Consider using CursorLoader to perform the query.
+                Cursor cursor = getContentResolver()
+                        .query(contactUri, projection, null, null, null);
+                cursor.moveToFirst();
+
+                // Retrieve the phone number from the NUMBER column
+                int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                String number = cursor.getString(column);
+
+                // Do something with the phone number...
+                Toast.makeText(this, number,Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    /**
+     * Before Android 2.3 (API level 9), performing a query on the Contacts Provider (like the one shown above)
+     * requires that your app declare the READ_CONTACTS permission (see Security and Permissions).
+     * However, beginning with Android 2.3, the Contacts/People app grants your app a temporary permission
+     * to read from the Contacts Provider when it returns you a result.
+     * The temporary permission applies only to the specific contact requested,
+     * so you cannot query a contact other than the one specified by the intent's Uri,
+     * unless you do declare the READ_CONTACTS permission.
+     */
+}
